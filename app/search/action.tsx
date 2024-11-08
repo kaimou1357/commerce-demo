1976e67810772c2356febd09b1d8a11ce48a0334
Kai Mou <kaimou1357@gmail.com>
2024-11-08 15:10:53 -0800

    Model Update

--- app/search/action.tsx
+++ app/search/action.tsx
@@ -2,7 +2,7 @@
 import { Product } from "lib/shopify/types";
 import OpenAI from "openai";
 import { zodResponseFormat } from 'openai/helpers/zod';
-import { ChatCompletionSystemMessageParam } from "openai/resources/chat/completions";
+import { ChatCompletionSystemMessageParam, ChatCompletionUserMessageParam } from "openai/resources/chat/completions";
 import { createClient } from "utils/supabase/server";
 import { z } from 'zod';
 
@@ -12,7 +12,6 @@ const FilteredProductsSchema = z.object({
 });
 
 interface SimplifiedProduct {
-  id: string,
   title: string,
   description: string,
   price: string,
@@ -21,30 +20,30 @@ interface SimplifiedProduct {
 
 export async function getFilteredProducts(products: Product[], searchQuery: string | undefined, reset: boolean): Promise<Product[]> {
   if (searchQuery === null || searchQuery === '') {
-    clearFiltersForUser();
+    clearPromptsForUser();
     return products;
   }
 
   if (reset) {
-    clearFiltersForUser();
+    clearPromptsForUser();
   }
 
   const simplifiedProducts = products.map(product => ({
-    id: product.id,
     title: product.handle,
     description: product.enhancedDescription?.value,
     price: product.priceRange.maxVariantPrice.amount,
   }));
 
-  var productPrompt = await getPreviousProductsForUser(simplifiedProducts)
-
+  var productPrompt = await getProductsPrompt(simplifiedProducts)
+  const prompts = await existingUserPrompts();
 
   const openai = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
 
   const chatCompletion = await openai.beta.chat.completions.parse({
     messages: [      
       productPrompt,
-      {role: "user", content: `The query is: ${searchQuery}`},
+      ...prompts,
+      {role: "user", content: `The new search query is: ${searchQuery}`},
       {role: "system", content: `
         You are an intelligent e-commerce sales agent specializing in understanding user intent and providing personalized shopping recommendations for clothing. Take into context the pre-existing user queries when making decisions to try to piece together their need.
         Your job is to carefully consider each user’s input, analyze their context (like formal vs casual events), and present only relevant results that match their specific needs.
@@ -52,11 +51,8 @@ export async function getFilteredProducts(products: Product[], searchQuery: stri
         For example, if they say ‘I’m going to get dirty at the wedding,’ switch to recommending less formal, more durable options, like casual outfits or clothing suited for outdoor or non-traditional settings.
         Always prioritize context over individual keywords. For example, the word ‘wedding’ doesn’t always mean formal, and the phrase ‘get dirty’ indicates they may want practical or casual clothing. 
         Your task is to balance the user’s stated purpose with their environment and give recommendations that fit both. The goal is to make the shopping experience feel like a conversation with a highly experienced sales agent who can understand subtle cues, anticipate needs, and provide tailored product suggestions. `},
-      {role: "system", content: "Please also include the reasoning of how you obtained the results in the reasoning field in the output"},
-      {role: "system", content: "When user filters on price, please just read the price column to determine the price to filter. Verify the price on each product - even if the names are similar. DO NOT INCLUDE ANYTHING ABOVE BUDGET"},
-      {role: "system", content: "In the output - return only the title field of the products that adhere to the user criteria."},
-      {role: "system", content: "please ensure that the filtered products list only contains the products after you applied your filters"}
-      
+      {role: "system", content: "Provide the filtered results in the filtered products field. Only include the handle of the product."},
+      {role: "system", content: "Using the user's previous prompts, use them as context when filtering. For example, if user already is searching for mens clothes, do not show women's clothing even if they search for something unisex like jackets"},
     ],
     model: "gpt-4o",
     temperature: 0,
@@ -67,46 +63,54 @@ export async function getFilteredProducts(products: Product[], searchQuery: stri
   const filteredProducts = results.filtered_products;
   console.log(results);
 
-  insertProductsForUser(filteredProducts);
+  await insertPromptForUser(searchQuery);
 
   return products.filter(product => filteredProducts.some(productName => productName === product.handle));
 }
 
-async function getPreviousProductsForUser(products: SimplifiedProduct[]): Promise<ChatCompletionSystemMessageParam>  {
-  const supabase = await createClient();
-  const {
-    data: { user }
-  } = await supabase.auth.getUser()
-
-  const { data , error} = await supabase.from('filtered_products').select().eq('owner_id', user?.id).limit(1)
-  if (error || data === null || data.length === 0) {
-    return {role: 'system', content: JSON.stringify(products)}
-  }
-
-  const record = data[0];
-  const previousFilteredProducts = record.products;
-  if (previousFilteredProducts.length === 0) {
-    return {role: 'system', content: JSON.stringify(products)}
-  }
-
-  const result = products.filter(product => previousFilteredProducts.some((productName: string) => productName === product.title));
-  return {role: 'system', content: `The starting product list you will be filtering from: ${JSON.stringify(result)}`};
+async function getProductsPrompt(products: SimplifiedProduct[]): Promise<ChatCompletionSystemMessageParam>  {
+  return {role: 'system', content: `All Products in the store: ${JSON.stringify(products)}`};
 }
 
-async function insertProductsForUser(productIds: string[]) {
+async function insertPromptForUser(prompt: string | undefined) {
   const supabase = await createClient();
   const {
     data: { user }
   } = await supabase.auth.getUser()
 
-  await supabase.from('filtered_products').upsert({owner_id: user?.id, products: productIds}, {onConflict: 'owner_id'});
+  const { data: promptData } = await supabase
+    .from('prompts')
+    .select('prompts')
+    .eq('user_id', user?.id)
+    .single()
+  
+  const updatedPrompts = promptData?.prompts ? [...promptData.prompts, prompt] : [prompt]
+
+  await supabase.from('prompts').upsert({user_id: user?.id, prompts: updatedPrompts}, {onConflict: 'user_id'});
 }
 
-async function clearFiltersForUser() {
+async function existingUserPrompts(): Promise<ChatCompletionUserMessageParam[]> {
   const supabase = await createClient();
   const {
     data: { user }
   } = await supabase.auth.getUser()
 
-  await supabase.from('filtered_products').update({products: []}).eq('owner_id', user?.id)
+  const {data, error}  = await supabase.from('prompts').select('prompts').eq('user_id', user?.id);
+
+  if (data === null || data.length === 0) {
+    return []
+  }
+
+  const prompts = data[0]?.prompts
+
+  return prompts.map((prompt: any) => ({role: "user", content: prompt }));
+}
+
+async function clearPromptsForUser() {
+  const supabase = await createClient();
+  const {
+    data: { user }
+  } = await supabase.auth.getUser()
+
+  await supabase.from('prompts').update({prompts: []}).eq('user_id', user?.id)
 }
